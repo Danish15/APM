53c6ef055c45575d13a9da4091f21b7466835ee6
Danish <Dohabib@wlu.ca>
2016-06-06 11:10:46 -0400

    Creating Component for Album View

---
+++ APM - Start/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'pm-products',
+	templateURL: 'app/products/product-list.component.html'
+	
+})
+export class ProductListComponent{
+	
+}
