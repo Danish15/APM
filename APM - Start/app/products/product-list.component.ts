cce70a4133ae8c1f4b67e81afb1f6b46d7ad2a65
Danish <Dohabib@wlu.ca>
2016-06-09 13:14:19 -0400

    Adding product list components

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -6,5 +6,52 @@ import {Component} from 'angular2/core';
 	
 })
 export class ProductListComponent{
-	
+	pageTitle: string = 'Album List';
+	products: any[] = [
+    {
+        "productId": 1,
+        "Name": "Leaf Rake",
+        "Artist": "GDN-0011",
+        "Genre": "March 19, 2016",
+        "Year": "Leaf rake with 48-inch wooden handle.",
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+        "productId": 2,
+   		"Name": "Leaf Rake",
+        "Artist": "GDN-0011",
+        "Genre": "March 19, 2016",
+        "Year": "Leaf rake with 48-inch wooden handle.",
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+        "productId": 5,
+      	"Name": "Leaf Rake",
+        "Artist": "GDN-0011",
+        "Genre": "March 19, 2016",
+        "Year": "Leaf rake with 48-inch wooden handle.",
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+        "productId": 8,
+       	"Name": "Leaf Rake",
+        "Artist": "GDN-0011",
+        "Genre": "March 19, 2016",
+        "Year": "Leaf rake with 48-inch wooden handle.",
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+        "productId": 10,
+       	"Name": "Leaf Rake",
+        "Artist": "GDN-0011",
+        "Genre": "March 19, 2016",
+        "Year": "Leaf rake with 48-inch wooden handle.",
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+];
 }
